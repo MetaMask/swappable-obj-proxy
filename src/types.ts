96e55248807005247b8eacbe6f00281cf36ae947
legobt <6wbvkn0j@anonaddy.me>
2024-01-16 20:11:08 +0000

    feat(types): export EventEmitterLike

--- src/types.ts
+++ src/types.ts
@@ -10,3 +10,20 @@
 export type SwappableProxy<Type extends object> = Type & {
   setTarget: (newTarget: Type) => void;
 };
+
+/**
+ * A portion of Node's EventEmitter interface that `createEventEmitterProxy`
+ * expects its `target` to support.
+ */
+export type EventEmitterLike = {
+  eventNames: () => (string | symbol)[];
+  // The `rawListeners` method returns an array of `Function`s.
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  rawListeners(eventName: string | symbol): Function[];
+  removeAllListeners(event?: string | symbol): EventEmitterLike;
+  on(name: string, handler: (...args: unknown[]) => unknown): void;
+  prependListener(name: string, handler: (...args: unknown[]) => unknown): void;
+  addListener(name: string, handler: (...args: unknown[]) => unknown): void;
+  off(name: string, handler: (...args: unknown[]) => unknown): void;
+  once(name: string, handler: (...args: unknown[]) => unknown): void;
+};
